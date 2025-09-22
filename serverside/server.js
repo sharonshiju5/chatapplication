6a95cc9697ab8c69d866a16ea244dd8c0ab2605b
sharonshiju5 <sharonshiju261@gmail.com>
2025-09-22 18:20:47 +0530

    server

--- serverside/server.js
+++ serverside/server.js
@@ -4,6 +4,7 @@ import { createServer } from "http";
 import connection from "./connection.js";
 import env from "dotenv";
 import router from "./router.js";
+import path from "path";
 import initializeSocket from "./socketServer.js";
 
 env.config();
@@ -18,9 +19,18 @@ app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use("/api", router);
 
+// Serve frontend (React build folder)
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Connect DB & start server
 connection().then(() => {
   const PORT = process.env.PORT || 5000; // fallback for local dev
-  app.listen(PORT, () => {
-    console.log(`🚀 Server started on http://localhost:${PORT}`);
+  httpServer.listen(PORT, () => {
+    console.log(`🚀 Server + Socket.IO started on http://localhost:${PORT}`);
   });
 });
